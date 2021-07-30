b115cf74da594466f21ddb8e5f4eefef61615b89
Daniel <d.tapialopez1@gmail.com>
2021-07-30 13:37:35 -0500

    Correcion de servicio de historiales

--- controller/historialCompra.js
+++ controller/historialCompra.js
@@ -6,7 +6,7 @@ const obtenerHistoriales = async (req, res = response) => {
     try {
         const historiales = await HistorialCompra.find({ usuario: usuario })
             .populate('usuario', 'email nombre_1 apellido_1').populate('compra', 'total', 'fecha');
-        return res.status(400).json({
+        return res.status(200).json({
             ok: true,
             historiales
         });
